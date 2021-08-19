3a9fbe57e42ccec395199cfa7219e2ac5d9a449e
oluwaseyikehinde <62704189+oluwaseyikehinde@users.noreply.github.com>
2021-08-19 23:50:32 +0100

    Console App
    Console App introducing myself written in C#

---
+++ MyConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello ! My name is Seyi Ajayi, I am a Frontend Developer and a UI Designer, This is my console App, i did this with C#");
+
+            Console.ReadKey();
+        }
+    }
+}
